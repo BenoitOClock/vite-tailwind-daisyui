c1d8f7af624793bfbd52058891329070751e88b9
Benoît Schiex <benoit@oclock.io>
2024-08-01 22:26:05 +0200

    fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -24,19 +24,3 @@ export default defineConfig({
     entries: "src/**/*{.html,.css,.js}",
   },
 });
-
-console.log(
-  Object.fromEntries(
-    globSync("src/**/*.html").map(file => [
-      // This remove `src/` as well as the file extension from each
-      // file, so e.g. src/nested/foo.js becomes nested/foo
-      path.relative(
-        "src",
-        file.slice(0, file.length - path.extname(file).length)
-      ),
-      // This expands the relative paths to absolute paths, so e.g.
-      // src/nested/foo becomes /project/src/nested/foo.js
-      fileURLToPath(new URL(file, import.meta.url)),
-    ])
-  )
-);
